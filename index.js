b01c3bc97b7b2d72d4e0d52727e5e1abdf58846d
denaindah <denaindahamalia@gmail.com>
2019-12-06 10:24:36 +0700

    kuis2

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Builder, By } = require("selenium-webdriver");
+
+async function googling() {
+  //buka browser
+  let driver = await new Builder().forBrowser("firefox").build();
+  let assert = require("assert");
+  // buka url google
+  try {
+    await driver.get("https://s2.demo.opensourcecms.com/orangehrm/symfony/web/index.php/auth/login");
+  
+  
+  await driver.findElement(By.name("txtUsername")).sendKeys("opensourcecms");
+  await driver.findElement(By.name("txtPassword")).sendKeys("opensourcecms");
+  await driver.findElement(By.name("Submit")).click();
+
+  const testcase = await driver.findElement(By.xpath('//li')).getText();
+  assert.equal(testcase, "Welcome Admin", "Failed");
+  Console.log("Success");
+  
+  }catch(error){
+    Console.log("failed")
+  }
+}
+googling();
